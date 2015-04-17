216b03e73bb04094a63cdfac9f59e43d72d4bbb3
Ryan Brown <ryan@ryanleebrown.com>
2015-04-17 14:41:01 -0700

    Document EndpointsController since the documentation was refactored out.

--- pkg/service/endpoints_controller.go
+++ pkg/service/endpoints_controller.go
@@ -24,10 +24,15 @@ import (
 	"github.com/golang/glog"
 )
 
+// An EndpointsController knows how to update a client's service
+// endpoints for a certain networking strategy (e.g. one ip per pod).
 type EndpointsController interface {
+	// SyncServiceEndpoints updates the client's service endpoints.
 	SyncServiceEndpoints() error
 }
 
+// A ControllerFunc creates an EndpointsController that updates the
+// given client.
 type ControllerFunc func(*client.Client) EndpointsController
 
 var (
@@ -35,16 +40,20 @@ var (
 	controllers     = make(map[string]ControllerFunc)
 )
 
+// RegisterEndpointsController registers a ControllerFunc by name.
 func RegisterEndpointsController(name string, f ControllerFunc) {
 	controllersLock.Lock()
 	defer controllersLock.Unlock()
 	if _, ok := controllers[name]; ok {
 		glog.Fatalf("Endpoints controller %q was registered twice", name)
 	}
-	glog.V(1).Infof("Registered cloud provider %q", name)
+	glog.V(1).Infof("Registered endpoints controller %q", name)
 	controllers[name] = f
 }
 
+// GetEndpointsController creates an EndpointsController using the
+// ControllerFunc registered to the given name. If there is no
+// ControllerFunc registered to the given name, it returns nil.
 func GetEndpointsController(name string, client *client.Client) EndpointsController {
 	controllersLock.Lock()
 	defer controllersLock.Unlock()
